5970ff13fce86e59a6bc5cc9d9ef877a49e7bc9c
André Luís Jaques <andreluisptj@gmail.com>
2024-11-06 18:02:50 -0300

    Update enviaform.php

--- enviaform.php
+++ enviaform.php
@@ -1,8 +1,8 @@
 <?php
 
 $nome = addcslashes($_POST['nome']);
-$email = addcslashes($_POST['emial']);
-$telefone = addcslashes($_POST['telefone']);
+$email = addcslashes($_POST['email']);
+$telefone = isset($_POST['telefone']) ? addcslashes($_POST['telefone']) : ''; // Opcional, se telefone estiver no formulário
 $mensagem = addcslashes($_POST['mensagem']);
 
 $para = "andreluisptj@gmail.com";
@@ -10,12 +10,12 @@ $assunto = "Coleta de dados";
 
 $corpo = "Nome: ".$nome."\n"."E-mail: ".$email."\n"."Telefone: ".$telefone."\n"."Mensagem: ".$mensagem;
 
-$cabeca = " From: andreluisptj@gmail.com"."\n"."Reply-to: ".$email."\n"."X=Mailer:PHP/".phpversion();
+$cabeca = "From: andreluisptj@gmail.com"."\n"."Reply-To: ".$email."\n"."X-Mailer: PHP/".phpversion();
 
-if(mail($para,$assunto,$corpo,$cabeca)){
-    echo("E-mail enviado com sucesso!");
+if(mail($para, $assunto, $corpo, $cabeca)){
+    echo "E-mail enviado com sucesso!";
 } else {
-    echo("Aconteceu um erro ao enviar o e-mail.");
+    echo "Aconteceu um erro ao enviar o e-mail.";
 }
 
 ?>
